9659e81d02be6ea749db2c7d258f678c508a0bea
Arunabha-NOTE <arunabham2017@gmail.com>
2024-07-14 19:10:21 +0530

    completed day2

---
+++ day2.js
@@ -0,0 +1,51 @@
+//Activity 1
+
+//Task-1
+let var1 = 10;
+let var2= 11;
+console.log(var1+var2);
+
+//Task-2
+console.log(var2-var1);
+
+//Task-3
+console.log(var2*var1);
+
+//Task-4 
+console.log(var2/var1);
+
+//Task-5
+console.log(var2%var1);
+
+//Activity-2
+//Task-6
+console.log(var1 += 3);
+
+//Task-7
+console.log(var2 -= 9);
+
+//Activity-3
+//Task-8
+console.log(var1 > var2);
+console.log(var1 < var2);
+
+//Task-9
+console.log(var1 >= var2);
+console.log(var1 <= var2);
+
+//Task-10
+console.log(var1 == var2);
+console.log(var1 === var2);
+
+//Activity-4
+//Task-11
+console.log(var1==var2 && var1 <= var2);
+
+//Task-12
+console.log(var1==var2 || var1 <= var2);
+
+//Task-13
+console.log(!(var1==var2));
+
+//Activity-5
+(var1==var2)?console.log("true"):console.log("false");
