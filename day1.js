717c84e4de07a5971889b0de5b0998857f94e3cc
Arunabha-NOTE <arunabham2017@gmail.com>
2024-07-14 18:49:21 +0530

    completed day1

--- day1.js
+++ day1.js
@@ -1,1 +1,53 @@
-console.log("hello");
+//Activity 1
+
+const { type } = require("express/lib/response");
+
+//part-1
+var variable1 = 9;
+console.log(variable1);
+
+//part-2
+let variable2 = "hello";
+console.log(variable2);
+
+//Activity 2
+const variable3 = boolean = true;
+console.log(variable3);
+
+//Activity-3
+let var4 = "Hello";
+console.log(typeof(var4));
+
+//Activity-4
+let var5 = 5;
+console.log(var5);
+var5 = 10;
+console.log(var5);
+
+//Activity-5
+// const var6 = 5; throws error
+// var6 = 5;
+
+//Feature Request 
+
+//first 
+let var6 = "Hello";
+console.log(typeof(var6));
+
+let var7 = 5;
+console.log(typeof(var7));
+
+let var8 = [4,5,6,7]
+console.log(typeof(var8));
+
+let var9 = boolean;
+var9 = true;
+console.log(typeof(var9));
+
+//Second
+let var10 = 5;
+var10 = 10;
+console.log(var10);
+
+// const var11 = 5; throws error
+// var11 = 24;
